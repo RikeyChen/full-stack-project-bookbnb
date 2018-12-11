7d500629a2cc85e9b4b028eea1843dc76b41d3d4
Rikey Chen <rikey.chen16@gmail.com>
2018-12-11 10:53:04 -0800

    Update users controller to include error messages

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,6 +7,7 @@ class Api::UsersController < ApplicationController
       login(@user) # APP CONTROLLER
       render 'api/users/show' # make JSON JBUILDER
     else
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
