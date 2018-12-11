4651e704bc5fee35668b183977fcbda1158caf90
Rikey Chen <rikey.chen16@gmail.com>
2018-12-10 22:02:53 -0800

    Add User model and table

---
+++ app/models/user.rb
@@ -0,0 +1,71 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  email           :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates
+    :email,
+    :first_name,
+    :last_name,
+    :password_digest,
+    :session_token,
+  presence: true
+
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  before_validation :ensure_session_token
+
+  # associations - need to fill out (prob not one-liners)
+  # has_many :bookings
+  # has_many :reviews
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.is_password?(password) ? user : password
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  def new_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def generate_unique_session_token
+    self.session_token = new_session_token
+    while User.find_by(session_token: self.session_token)
+      self.session_token = new_session_token
+    end
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token || generate_unique_session_token
+  end
+
+end
