9873e11dfd8f83d7dac1bdb875f815105c81990b
Rikey Chen <rikey.chen16@gmail.com>
2018-12-11 14:51:53 -0800

    Create store and add to entry file

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 import {
   login,
-  logout,
   signup,
-} from './util/session_api_util';
+  logout,
+} from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  // const store for later
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.login = login;
-  window.logout = logout;
   window.signup = signup;
+  window.logout = logout;
   ReactDOM.render(<h1>REACT IS WORKING!</h1>, root);
-})
-  ;
+});
