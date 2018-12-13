5f83a5b3790392f0caf554a07b50bb31be897708
Rikey Chen <rikey.chen16@gmail.com>
2018-12-13 10:59:06 -0800

    Add listings reducer

---
+++ frontend/reducers/listings_reducer.js
@@ -0,0 +1,17 @@
+import { merge } from 'lodash';
+import { RECEIVE_LISTINGS, RECEIVE_LISTING } from '../actions/listing_actions';
+
+const listingsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  switch (action.type) {
+    case RECEIVE_LISTINGS:
+      return action.listings;
+    case RECEIVE_LISTING:
+      const newListing = { [action.listing.id]: action.listing };
+      return merge({}, oldState, newListing);
+    default:
+      return oldState;
+  }
+};
+
+export default listingsReducer;
