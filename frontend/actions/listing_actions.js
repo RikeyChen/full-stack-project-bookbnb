4cbf041f36c5b331ccbecd57deabf3f666866300
Rikey Chen <rikey.chen16@gmail.com>
2018-12-13 10:51:06 -0800

    Add listing action creators

---
+++ frontend/actions/listing_actions.js
@@ -0,0 +1,24 @@
+import * as ListingApiUtil from '../util/listing_api_util';
+
+export const RECEIVE_LISTINGS = 'RECEIVE_LISTINGS';
+export const RECEIVE_LISTING = 'RECEIVE_LISTING';
+
+export const receiveListings = listings => ({
+  type: RECEIVE_LISTINGS,
+  listings,
+});
+
+export const receiveListing = listing => ({
+  type: RECEIVE_LISTING,
+  listing,
+});
+
+export const fetchListings = () => dispatch => (
+  ListingApiUtil.fetchListings()
+    .then(listings => dispatch(receiveListings(listings)))
+);
+
+export const fetchListing = listingId => dispatch => (
+  ListingApiUtil.fetchListing(listingId)
+    .then(listing => dispatch(receiveListing(listing)))
+);
