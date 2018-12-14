102f859f217c930232c8a8ae4e1f1d6fd5f5a0d6
Rikey Chen <rikey.chen16@gmail.com>
2018-12-14 15:59:58 -0800

    Add additional details for ListingDetail header section

--- frontend/components/listings/listings_show/listing_detail.jsx
+++ frontend/components/listings/listings_show/listing_detail.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 
 const ListingDetail = ({ listing }) => (
-  <div>
-    <header className="listing-detail-header">
-      <div className="listing-detail-header-main">
-        <div className="listing-detail-title-city">
-          <h1>{listing.name}</h1>
-          <span>{listing.city}</span>
+  <div className="listing-detail-master">
+    <div className="listing-detail-main-section">
+      <header className="listing-detail-header">
+        <div className="listing-detail-header-main">
+          <div className="listing-detail-title-city">
+            <h1>{listing.name}</h1>
+            <span>{listing.city}</span>
+          </div>
+          <div className="listing-detail-host-pic">
+            <img src="" alt="Host Profile" />
+          </div>
         </div>
-        <div className="listing-detail-host-pic">
-          <img src="" alt="Host Profile" />
+        <div className="listing-detail-core-amenities">
+          <span>
+            {listing.max_guests}
+            {listing.max_guests > 1 ? 'guests' : 'guest'}
+          </span>
+          <span>
+            {listing.num_bedrooms}
+            {listing.num_bedrooms > 1 ? 'bedrooms' : 'bedroom'}
+          </span>
+          <span>
+            {listing.num_beds}
+            {listing.num_beds > 1 ? 'beds' : 'bed'}
+          </span>
+          <span>
+            {listing.num_bathrooms}
+            bath
+          </span>
         </div>
-      </div>
-      <div className="listing-detail-core-amenities">
-        <span>
-          {listing.max_guests}
-          {listing.max_guests > 1 ? 'guests' : 'guest'}
-        </span>
-        <span>
-          {listing.num_bedrooms}
-          {listing.num_bedrooms > 1 ? 'bedrooms' : 'bedroom'}
-        </span>
-        <span>
-          {listing.num_beds}
-          {listing.num_beds > 1 ? 'beds' : 'bed'}
-        </span>
-        <span>
-          {listing.num_bathrooms}
-          bath
-        </span>
-
-      </div>
-    </header>
+      </header>
+    </div>
   </div>
 );
 
