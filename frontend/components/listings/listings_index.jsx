15b8c4c8a52e077d404f8bda2d1c2d5919cc329d
Rikey Chen <rikey.chen16@gmail.com>
2018-12-15 23:50:40 -0800

    Add initial structure for  side container for listings index

---
+++ frontend/components/listings/listings_index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import 'react-dates/initialize';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+class ListingsIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      price: null,
+      startDate: null,
+      endDate: null,
+      focusedInput: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="listings-index-master">
+        <aside>
+          <span>Dates</span>
+          <DateRangePicker
+            startDateId="startDate"
+            endDateId="endDate"
+            startDate={this.state.startDate}
+            endDate={this.state.endDate}
+            onDatesChange={({ startDate, endDate }) => { this.setState({ startDate, endDate }); }}
+            focusedInput={this.state.focusedInput}
+            onFocusChange={(focusedInput) => { this.setState({ focusedInput }); }}
+            hideKeyboardShortcutsPanel
+          />
+          <span>Guests</span>
+          <button id="booking-form-guests-dropdown">
+            {/* <option default selected disabled>1 guest</option>
+            <option value="adults">Adults</option>
+            <option value="children">Children Ages 2-12</option>
+            <option value="infants">Infants Under 2</option> */}
+            1 guest
+          </button>
+        </aside>
+      </div>
+    );
+  }
+}
