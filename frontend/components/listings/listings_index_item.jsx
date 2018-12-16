534c328026042b68e9bfabb6e1b62750661913da
Rikey Chen <rikey.chen16@gmail.com>
2018-12-16 12:02:42 -0800

    Add initial structure for ListingsIndexItem

---
+++ frontend/components/listings/listings_index_item.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const ListingsIndexItem = ({ listings }) => (
+  <div>
+    {listings.map(listing => (
+      <div>
+        <img src="" alt="" />
+        <span>lodging type</span>
+        <h1>Listing name</h1>
+        <div>
+          <div>$</div>
+          <div>price</div>
+          <span>per night</span>
+          <span>Free cancellation</span>
+        </div>
+        <div className="booking-form-stars">
+          <div className="stars-outer">
+            <div className="stars-inner" />
+          </div>
+          <div id="booking-form-review-count">300</div>
+        </div>
+      </div>
+    ))}
+  </div>
+)
+  ;
