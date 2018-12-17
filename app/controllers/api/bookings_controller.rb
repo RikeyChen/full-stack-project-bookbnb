516d9ae8406295ce2c3424fb95d47258e6fce026
Rikey Chen <rikey.chen16@gmail.com>
2018-12-17 10:48:04 -0800

    Add bookings controller

---
+++ app/controllers/api/bookings_controller.rb
@@ -0,0 +1,17 @@
+class Api::BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.guest_id = current_user.id
+    @booking.listing_id = params.id
+
+    if @booking.save
+      render :show
+    else
+      render json: @booking.errors.full_messages, status: 422
+    end
+  end
+
+  def booking_params
+    params.require(:booking).permit(:checkin_date, :checkout_date, :num_guests)
+  end
+end
