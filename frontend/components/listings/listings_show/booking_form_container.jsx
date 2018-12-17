bb953d9236fce23bc58d361b505c140dc2c141bd
Rikey Chen <rikey.chen16@gmail.com>
2018-12-17 15:41:52 -0800

    Add withRouter to pass ownProps

--- frontend/components/listings/listings_show/booking_form_container.jsx
+++ frontend/components/listings/listings_show/booking_form_container.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import BookingForm from './booking_form';
 import createBooking from '../../../actions/booking_actions';
 
-const mSp = (state, ownProps) => ({
-  listing: state.entities.listings[ownProps.match.params.listingId],
-  bookings: state.entities.bookings,
-});
+const mSp = (state, ownProps) => {
+  debugger;
+  return ({
+    listing: state.entities.listings[ownProps.match.params.listingId],
+    bookings: state.entities.bookings,
+    price: ownProps.price,
+  });
+};
 
 const mDp = dispatch => ({
   createBooking: (listingId, booking) => dispatch(createBooking(listingId, booking)),
 });
 
-export default connect(mSp, mDp)(BookingForm);
+export default withRouter(connect(mSp, mDp)(BookingForm));
