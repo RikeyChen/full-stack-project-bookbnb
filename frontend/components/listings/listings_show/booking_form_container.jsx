889e5a42a642cb858c3b14b31ede5136487a3ab8
Rikey Chen <rikey.chen16@gmail.com>
2018-12-17 15:21:10 -0800

    Add mSp and mDp

--- frontend/components/listings/listings_show/booking_form_container.jsx
+++ frontend/components/listings/listings_show/booking_form_container.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import BookingForm from './booking_form';
+import createBooking from '../../../actions/booking_actions';
 
 const mSp = state => ({
   bookings: state.entities.bookings,
+});
+
+const mDp = dispatch => ({
+  createBooking: (listingId, booking) => dispatch(createBooking(listingId, booking)),
 });
 
 export default connect(mSp, mDp)(BookingForm);
