318c23749231487998f02b4b08055947a204e2a9
Rikey Chen <rikey.chen16@gmail.com>
2018-12-18 14:02:57 -0800

    Update receive errors to be receiveSessionErrors to distinguish from booking errors

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as SessionApiUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 export const CLEAR_ERRORS = 'CLEAR ERRORS';
 
 export const receiveCurrentUser = user => ({
@@ -14,8 +14,8 @@ export const logoutCurrentUser = () => ({
   type: LOGOUT_CURRENT_USER,
 });
 
-export const receiveErrors = errors => ({
-  type: RECEIVE_ERRORS,
+export const receiveSessionErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
   errors,
 });
 
@@ -27,7 +27,7 @@ export const login = user => dispatch => (
   SessionApiUtil.login(user)
     .then(
       user => dispatch(receiveCurrentUser(user)),
-      errors => dispatch(receiveErrors(errors.responseJSON)),
+      errors => dispatch(receiveSessionErrors(errors.responseJSON)),
     )
 );
 
@@ -40,6 +40,6 @@ export const signup = user => dispatch => (
   SessionApiUtil.signup(user)
     .then(
       user => dispatch(receiveCurrentUser(user)),
-      errors => dispatch(receiveErrors(errors.responseJSON)),
+      errors => dispatch(receiveSessionErrors(errors.responseJSON)),
     )
 );
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,9 +1,10 @@
-import { RECEIVE_ERRORS, RECEIVE_CURRENT_USER, CLEAR_ERRORS } from '../actions/session_actions';
+import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER, CLEAR_ERRORS } from '../actions/session_actions';
 
 const sessionErrorsReducer = (oldState = [], action) => {
   Object.freeze(oldState);
   switch (action.type) {
-    case RECEIVE_ERRORS:
+    case RECEIVE_SESSION_ERRORS:
+      debugger;
       return action.errors;
     case RECEIVE_CURRENT_USER:
       return [];
