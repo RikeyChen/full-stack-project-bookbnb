8f67515c9e722e0007ccd2d0937a9a37c86e15bc
Rikey Chen <rikey.chen16@gmail.com>
2018-12-18 14:06:16 -0800

    Remove debugger

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -4,7 +4,6 @@ const sessionErrorsReducer = (oldState = [], action) => {
   Object.freeze(oldState);
   switch (action.type) {
     case RECEIVE_SESSION_ERRORS:
-      debugger;
       return action.errors;
     case RECEIVE_CURRENT_USER:
       return [];
