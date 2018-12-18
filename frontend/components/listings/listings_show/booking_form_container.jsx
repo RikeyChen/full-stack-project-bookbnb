da1bd92a9b3fb0bcdac9d2e555434d7b39f2f220
Rikey Chen <rikey.chen16@gmail.com>
2018-12-17 16:10:48 -0800

    Remove dubgger and refactor

--- frontend/components/listings/listings_show/booking_form_container.jsx
+++ frontend/components/listings/listings_show/booking_form_container.jsx
@@ -4,14 +4,11 @@ import { withRouter } from 'react-router-dom';
 import BookingForm from './booking_form';
 import createBooking from '../../../actions/booking_actions';
 
-const mSp = (state, ownProps) => {
-  debugger;
-  return ({
-    listing: state.entities.listings[ownProps.match.params.listingId],
-    bookings: state.entities.bookings,
-    price: ownProps.price,
-  });
-};
+const mSp = (state, ownProps) => ({
+  listing: state.entities.listings[ownProps.match.params.listingId],
+  bookings: state.entities.bookings,
+  price: ownProps.price,
+});
 
 const mDp = dispatch => ({
   createBooking: (listingId, booking) => dispatch(createBooking(listingId, booking)),
