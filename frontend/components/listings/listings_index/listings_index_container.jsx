2c14106fa436770aa3bea596070ccc8e82384568
Rikey Chen <rikey.chen16@gmail.com>
2018-12-19 08:30:42 -0800

    Add updateBounds to mDp

--- frontend/components/listings/listings_index/listings_index_container.jsx
+++ frontend/components/listings/listings_index/listings_index_container.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ListingsIndex from './listings_index';
 import { fetchListings } from '../../../actions/listing_actions';
+import { updateBounds } from '../../../actions/filter_actions';
 
 const mSp = state => ({
   listings: Object.values(state.entities.listings),
@@ -9,6 +10,7 @@ const mSp = state => ({
 
 const mDp = dispatch => ({
   fetchListings: () => dispatch(fetchListings()),
+  updateBounds: bounds => dispatch(updateBounds(bounds)),
 });
 
 export default connect(mSp, mDp)(ListingsIndex);
