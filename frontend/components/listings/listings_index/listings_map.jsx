6839a12767df50d28454e1c4e025dae15e7e4389
Rikey Chen <rikey.chen16@gmail.com>
2018-12-18 20:46:27 -0800

    Add render and componentDidMount

--- frontend/components/listings/listings_index/listings_map.jsx
+++ frontend/components/listings/listings_index/listings_map.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
-const ListingsMap = () => (
-  <div id="map-container" ref="map" />
-);
+class ListingsMap extends React.component {
+  componentDidMount() {
+    const mapOptions = {
+      center: { lat: 37.7758, lng: -122.435 },
+      zoom: 13,
+    };
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+  }
 
+  render() {
+    return (
+      <div ref={map => this.mapNode = map} />
+    );
+  }
+}
+
+export default ListingsMap;
