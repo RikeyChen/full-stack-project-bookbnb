927ce4cab7942de2ae6a3684b8f851f148bbd1c2
Rikey Chen <rikey.chen16@gmail.com>
2018-12-19 08:52:45 -0800

    Add filters

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import modal from './modal_reducer';
+import filters from './filters_reducer';
 
 export default combineReducers({
   modal,
+  filters,
 });
