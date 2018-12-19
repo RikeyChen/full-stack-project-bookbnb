4bfb57cae40967d90d172acd411d270ae749ee62
Rikey Chen <rikey.chen16@gmail.com>
2018-12-19 08:41:36 -0800

    Change code to use updateFilter thunk action creator

--- frontend/actions/filter_actions.js
+++ frontend/actions/filter_actions.js
@@ -1,6 +1,14 @@
-export const UPDATE_BOUNDS = 'UPDATE_BOUNDS';
+import { fetchListings } from './listing_actions';
 
-export const updateBounds = bounds => ({
-  type: UPDATE_BOUNDS,
-  bounds,
+export const UPDATE_FILTER = 'UPDATE_FILTER';
+
+export const changeFilter = (filter, value) => ({
+  type: UPDATE_FILTER,
+  filter,
+  value,
 });
+
+export const updateFilter = (filter, value) => (dispatch, getState) => {
+  dispatch(changeFilter(filter, value));
+  return fetchListings(getState().ui.filters)(dispatch);
+};
