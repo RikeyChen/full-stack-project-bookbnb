7bda1676fd05e2038961643d7191ad65ccc61ab3
Rikey Chen <rikey.chen16@gmail.com>
2018-12-18 17:11:56 -0800

    Add clearErrors

--- frontend/components/navbar/navbar_container.jsx
+++ frontend/components/navbar/navbar_container.jsx
@@ -4,6 +4,7 @@ import { withRouter } from 'react-router-dom';
 import NavBar from './navbar';
 import { logout, clearSessionErrors } from '../../actions/session_actions';
 import { openModal } from '../../actions/modal_actions';
+import { clearErrors } from '../../actions/booking_actions';
 
 const mSp = state => ({
   currentUser: state.entities.users[state.session.id],
@@ -13,6 +14,7 @@ const mDp = dispatch => ({
   logout: () => dispatch(logout()),
   openModal: modal => dispatch(openModal(modal)),
   clearSessionErrors: () => dispatch(clearSessionErrors()),
+  clearErrors: () => dispatch(clearErrors()),
 });
 
 export default withRouter(connect(mSp, mDp)(NavBar));
