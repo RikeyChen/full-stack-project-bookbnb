2069c0428b340b781156eeba42d7665f1a41a8b8
Rikey Chen <rikey.chen16@gmail.com>
2018-12-19 08:11:26 -0800

    Add ::in_bounds

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -43,4 +43,11 @@ class Listing < ApplicationRecord
 
   # has an average rating through reviews
 
+  def listing.in_bounds(bounds)
+    self.where("lat < ?", bounds[:northEast][:lat])
+      .where("lat > ?", bounds[:southWest][:lat])
+      .where("lng > ?", bounds[:southWest][:lng])
+      .where("lng < ?", bounds[:northEast][:lng])
+  end
+
 end
