9346e74f73c2ca3a3e906829cc2bc41ea1246794
Rikey Chen <rikey.chen16@gmail.com>
2018-12-19 08:20:02 -0800

    Add conditional check for bounds otherwise grab all listings

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,13 +1,17 @@
 class Api::ListingsController < ApplicationController
   def show
-    # @listing = Listing.find(params[:id])
     @listing = Listing.with_attached_images.find(params[:id])
     @user = @listing.host
     render 'api/listings/show'
   end
 
   def index
-    @listings = Listing.in_bounds(params[:bounds])
+    @listings = bounds ? Listing.in_bounds(bounds) : Listing.all
+
     render 'api/listings/index'
+  end
+
+  def bounds
+    params[:bounds]
   end
 end
