3e1344b5c45829d6b9e47fcb2f58526ee7bb6702
Rikey Chen <rikey.chen16@gmail.com>
2018-12-18 21:54:57 -0800

    Fix component to capitalized

--- frontend/components/listings/listings_map/listings_map.jsx
+++ frontend/components/listings/listings_map/listings_map.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class ListingsMap extends React.component {
+class ListingsMap extends React.Component {
   componentDidMount() {
     const mapOptions = {
       center: { lat: 37.7758, lng: -122.435 },
