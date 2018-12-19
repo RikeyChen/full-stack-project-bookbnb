dfca835a13b19107cfa58f7c9898118a9be6df61
Rikey Chen <rikey.chen16@gmail.com>
2018-12-19 08:51:23 -0800

    Initial filtersReducer creation

---
+++ frontend/reducers/filters_reducer.js
@@ -0,0 +1,18 @@
+import { merge } from 'lodash';
+import { UPDATE_FILTER } from '../actions/filter_actions';
+
+const defaultFilters = Object.freeze({
+  bounds: {},
+});
+
+const filtersReducer = (oldState = defaultFilters, action) => {
+  Object.freeze(oldState);
+  switch (action.type) {
+    case UPDATE_FILTER:
+      const newFilter = { [action.filter]: action.value }; return merge({}, oldState, newFilter);
+    default:
+      return oldState;
+  }
+};
+
+export default filtersReducer;
