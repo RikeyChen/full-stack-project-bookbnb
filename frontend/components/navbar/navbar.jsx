ffb1b6ccdb05577afed5e2553efe2cb4bd14cde2
Rikey Chen <rikey.chen16@gmail.com>
2018-12-19 11:04:28 -0800

    Convert to class component

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -2,71 +2,79 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { clearErrors } from '../../actions/booking_actions';
 
-const NavBar = ({
-  currentUser, logout, openModal, location, clearSessionErrors, clearErrors,
-}) => {
-  let searchBar;
-  if (location.pathname === '/') {
-    searchBar = null;
-  } else {
-    searchBar = (<input type="text" placeholder={'Try "San Francisco"'} />);
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { city: null };
   }
 
-  let display;
-  if (currentUser === undefined) {
-    display = (
-      <div className="navbar-search-links-innerdiv">
-        <nav className="navbar-search">
-          {searchBar}
-        </nav>
-        <nav className={location.pathname === '/' ? 'navbar-links-splash' : 'navbar-links'}>
-          <button>Become a host</button>
-          <button>Help</button>
-          <button onClick={() => {
-            openModal('Sign up');
-            clearSessionErrors();
-          }}
-          >
-            Sign up
-          </button>
-          <button onClick={() => {
-            openModal('Log in');
-            clearSessionErrors();
-          }}
-          >
-            Log in
-          </button>
-        </nav>
-      </div>
-    );
-  } else {
-    display = (
-      <div className="navbar-search-links-innerdiv">
-        <nav className="navbar-search">
-          {searchBar}
-        </nav>
-        <nav className="navbar-links">
-          <button>Become a host</button>
-          <button>Trips</button>
-          <button>Help</button>
-          <button onClick={() => {
-            logout();
-            clearErrors();
-          }}
-          >
-            Logout
+  render() {
+    const {
+      currentUser, logout, openModal, location, clearSessionErrors, clearErrors,
+    } = this.props;
 
-          </button>
-        </nav>
+    let searchBar;
+    if (location.pathname === '/') {
+      searchBar = null;
+    } else {
+      searchBar = (<input type="text" placeholder={'Try "San Francisco"'} />);
+    }
+
+    let display;
+    if (currentUser === undefined) {
+      display = (
+        <div className="navbar-search-links-innerdiv">
+          <nav className="navbar-search">
+            {searchBar}
+          </nav>
+          <nav className={location.pathname === '/' ? 'navbar-links-splash' : 'navbar-links'}>
+            <button>Become a host</button>
+            <button>Help</button>
+            <button onClick={() => {
+              openModal('Sign up');
+              clearSessionErrors();
+            }}
+            >
+              Sign up
+            </button>
+            <button onClick={() => {
+              openModal('Log in');
+              clearSessionErrors();
+            }}
+            >
+              Log in
+            </button>
+          </nav>
+        </div>
+      );
+    } else {
+      display = (
+        <div className="navbar-search-links-innerdiv">
+          <nav className="navbar-search">
+            {searchBar}
+          </nav>
+          <nav className="navbar-links">
+            <button>Become a host</button>
+            <button>Trips</button>
+            <button>Help</button>
+            <button onClick={() => {
+              logout();
+              clearErrors();
+            }}
+            >
+              Logout
+
+            </button>
+          </nav>
+        </div>
+      );
+    }
+
+    return (
+      <div className="navbar-search-links-outterdiv">
+        {display}
       </div>
     );
   }
-
-  return (
-    <div className="navbar-search-links-outterdiv">
-      {display}
-    </div>
-  );
-};
-
+}
 export default NavBar;
