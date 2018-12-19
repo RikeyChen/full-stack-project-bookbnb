3dbbc090778d2c6583eb924e3bd33f1d53efefc4
Rikey Chen <rikey.chen16@gmail.com>
2018-12-18 22:18:29 -0800

    Add #removeMarker

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -28,4 +28,9 @@ export default class MarkerManager {
     marker.addListener('click', () => this.handleClick(listing));
     this.markers[marker.listingId] = marker;
   }
+
+  removeMarker(marker) {
+    this.markers[marker.listingId].setMap(null);
+    delete this.markers[marker.listingId];
+  }
 }
