37806b1af5b3905a6ad19de5d8c7423e2d476310
Rikey Chen <rikey.chen16@gmail.com>
2018-12-19 19:57:51 -0800

    Moved ListingsIndexSidebar to its own component file

--- frontend/components/listings/listings_index/listings_index.jsx
+++ frontend/components/listings/listings_index/listings_index.jsx
@@ -1,55 +1,13 @@
 import React from 'react';
-import 'react-dates/initialize';
-import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
 import ListingsIndexItem from './listings_index_item';
 import ListingsMap from '../listings_map/listings_map';
+import ListingsIndexSidebar from './listings_index_sidebar';
 
 class ListingsIndex extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      price: null,
-      startDate: null,
-      endDate: null,
-      focusedInput: null,
-    };
-  }
-
   render() {
     return (
       <div className="listings-index-master">
-        <aside className="listings-index-sidebar">
-          <div className="sidebar-dates">
-            <span>Dates</span>
-            <DateRangePicker
-              startDateId="startDate"
-              endDateId="endDate"
-              startDate={this.state.startDate}
-              endDate={this.state.endDate}
-              onDatesChange={({ startDate, endDate }) => { this.setState({ startDate, endDate }); }}
-              focusedInput={this.state.focusedInput}
-              onFocusChange={(focusedInput) => { this.setState({ focusedInput }); }}
-              hideKeyboardShortcutsPanel
-              small
-              showClearDates
-              reopenPickerOnClearDates
-              startDatePlaceholderText="Check in"
-              endDatePlaceholderText="Check out"
-            />
-          </div>
-          <span>Guests</span>
-          <button id="booking-form-guests-dropdown">
-            {/* <option default selected disabled>1 guest</option>
-            <option value="adults">Adults</option>
-            <option value="children">Children Ages 2-12</option>
-            <option value="infants">Infants Under 2</option> */}
-            1 guest
-          </button>
-          <hr />
-          <span>Price range</span>
-          <div>Price slider goes here</div>
-        </aside>
+        <ListingsIndexSidebar />
         <div className="listings-index-map-container">
           <main className="listings-index-main">
             <header>300+ homes</header>
