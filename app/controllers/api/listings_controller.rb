ce97b15ba80767d367a33d4b41562e12a276aca9
Rikey Chen <rikey.chen16@gmail.com>
2018-12-21 12:16:53 -0800

    Refactor to connect application state to filters on index page

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -7,25 +7,6 @@ class Api::ListingsController < ApplicationController
 
   def index
     @listings = bounds ? Listing.in_bounds(bounds) : Listing.all
-    # if start_date && end_date
-    #   @listings = @listings.select do |listing|
-    #     !unavailable_dates(listing).include?(start_date) ||
-    #     !unavailable_dates(listing).include?(end_date)
-    #   end
-    # end
-
-    # if start_date && end_date
-    #   sql =
-    #   "SELECT * from listings
-    #   WHERE"
-    #   @listings = @listings
-    #     .where.not('checkin_date IN ')
-    # end
-
-    @listings =
-      @listings
-        .where("max_guests >= ?", guests)
-        .where("price <= ?", max_price)
 
     render 'api/listings/index'
   end
@@ -41,21 +22,5 @@ class Api::ListingsController < ApplicationController
   def max_price
     params[:max_price] || 1000
   end
-
-  # def start_date
-  #   Date.parse(params[:dates][:start_date]) if params[:dates]
-  # end
-
-  # def end_date
-  #   Date.parse(params[:dates][:end_date]) if params[:dates]
-  # end
-
-  # def unavailable_dates(listing)
-  #   dates = []
-  #   listing.bookings.each do |booking|
-  #     dates += booking.unavailable_dates
-  #   end
-  #   dates
-  # end
 
 end
