21f2191049eec0bf1784551a36930e8b9d04baf7
Rikey Chen <rikey.chen16@gmail.com>
2018-12-23 18:10:19 -0800

    Add clear filters

--- frontend/actions/filter_actions.js
+++ frontend/actions/filter_actions.js
@@ -1,11 +1,16 @@
 import { fetchListings } from './listing_actions';
 
 export const UPDATE_FILTER = 'UPDATE_FILTER';
+export const CLEAR_FILTERS = 'CLEAR_FILTERS';
 
 export const changeFilter = (filter, value) => ({
   type: UPDATE_FILTER,
   filter,
   value,
+});
+
+export const clearFilters = () => ({
+  type: CLEAR_FILTERS,
 });
 
 export const updateFilter = (filter, value) => (dispatch, getState) => {
--- frontend/reducers/filters_reducer.js
+++ frontend/reducers/filters_reducer.js
@@ -1,8 +1,11 @@
 import { merge } from 'lodash';
-import { UPDATE_FILTER } from '../actions/filter_actions';
+import { UPDATE_FILTER, CLEAR_FILTERS } from '../actions/filter_actions';
 
 const defaultFilters = Object.freeze({
-  bounds: {},
+  bounds: {
+    northEast: { lat: 37.82781631655672, lng: -122.40538841247559 },
+    southWest: { lat: 37.72374705944836, lng: -122.46461158752442 },
+  },
   guests: 1,
   max_price: 1000,
   dates: {},
@@ -12,7 +15,10 @@ const filtersReducer = (oldState = defaultFilters, action) => {
   Object.freeze(oldState);
   switch (action.type) {
     case UPDATE_FILTER:
-      const newFilter = { [action.filter]: action.value }; return merge({}, oldState, newFilter);
+      const newFilter = { [action.filter]: action.value };
+      return merge({}, oldState, newFilter);
+    case CLEAR_FILTERS:
+      return defaultFilters;
     default:
       return oldState;
   }
