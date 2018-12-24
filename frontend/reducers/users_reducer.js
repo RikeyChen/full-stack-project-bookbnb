6becb3c49f42f356e8a7d2818e741f8046970df1
Rikey Chen <rikey.chen16@gmail.com>
2018-12-24 15:02:36 -0800

    Add RECEIVE_USER action type

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,12 +1,15 @@
 import { merge } from 'lodash';
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 import { RECEIVE_LISTING } from '../actions/listing_actions';
+import { RECEIVE_USER } from '../actions/user_actions';
 
 const usersReducer = (oldState = {}, action) => {
   Object.freeze(oldState);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return merge({}, oldState, { [action.user.id]: action.user });
+    case RECEIVE_USER:
+      return action.user;
     case RECEIVE_LISTING:
       return merge({}, oldState, { [action.user.id]: action.user });
     default:
