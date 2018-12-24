1a3e9c50a2f0cbb21713284a92aeb91bd6738775
Rikey Chen <rikey.chen16@gmail.com>
2018-12-23 23:32:41 -0800

    Update users show action to index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :show]
+    resources :users, only: [:create, :index]
     resource :session, only: [:create, :destroy, :show]
     resources :listings, only: [:index, :show] do
       resources :bookings, only: [:create]
