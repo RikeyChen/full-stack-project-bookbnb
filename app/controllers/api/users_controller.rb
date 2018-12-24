12225c1f197c852b898934f6866748aab59c8ad0
Rikey Chen <rikey.chen16@gmail.com>
2018-12-24 13:54:09 -0800

    Add getaways to index action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,6 +3,7 @@ class Api::UsersController < ApplicationController
   def index
     @user = User.find(current_user.id)
     @bookings = @user.bookings
+    @getaways = @user.getaways
     render 'api/users/show'
   end
 
